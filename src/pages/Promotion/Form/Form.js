38cf6f569342a6e9264f9ea9931ac8c7e41c87c0
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2021-07-18 23:52:46 -0300

    saved file form

--- src/pages/Promotion/Form/Form.js
+++ src/pages/Promotion/Form/Form.js
@@ -6,16 +6,6 @@ import UIContainer from 'components/UI/Container/Container'
 const PagesPromotionForm = () => {
     const { id } = useParams();
 
-
-    const promotion = {
-        "title": "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
-        "price": 1799,
-        "url": "",
-        "imageUrl": "https://cdn.gatry.com/gatry-static/promocao/imagem_url/2631517face1861bc4f46ae154d387de.png"
-    };
-
-
-
     return (
         <UIContainer>
             <PromotionForm />
